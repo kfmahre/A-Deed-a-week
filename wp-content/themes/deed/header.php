60669bab489fa9a15e0bb70537c4d07460a7dbc9
Ruthie Hassan <rhassan@findscout.com>
2016-06-04 17:11:59 -0400

    add google analytics

--- wp-content/themes/deed/header.php
+++ wp-content/themes/deed/header.php
@@ -25,6 +25,19 @@
 	window.isLoggedIn = <?php is_User_Logged_In() ?>
 	</script>
   	<?php // end facebook ?>
+
+  	<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-78766846-1', 'auto');
+		  ga('require', 'displayfeatures');
+			ga('require', 'linkid');
+		  ga('send', 'pageview');
+
+		</script>
 </head>
 <body <?php body_class(); ?>>
 <header class="container header">
