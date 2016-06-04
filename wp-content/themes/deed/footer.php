88373f6f420e5e057cf5fc0d94e6ff8f773819ac
wbhob <wilsonhobbs1@gmail.com>
2016-06-04 16:54:47 -0400

    easter egg

--- wp-content/themes/deed/footer.php
+++ wp-content/themes/deed/footer.php
@@ -1,20 +1,22 @@
-		</div><!-- END CONTAINER -->  
-		</div><!-- END FLUID CONTAINER -->	
-		
+		</div><!-- END CONTAINER -->
+		</div><!-- END FLUID CONTAINER -->
+
+		<A HREF="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
 		<footer class="container-fluid">
 			<div class="container">
 				<div class="row-fluid">
 					<div class="span5">
-					
+
 					</div>
 					<div class="span5 offset2">
-					
+
 					</div>
 				</div>
 			</div>
-		</footer>  
+		</footer>
+		</A>
     <!-- WP FOOTER -->
   	<?php wp_footer(); ?>
-	    
+
 </body>
 </html>
