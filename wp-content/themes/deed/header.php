589192c7edaa9d018b26cac0697183058f71b2a9
kfmahre <kfmahre@gmail.com>
2016-06-16 19:16:38 -0400

    deleting some useless divs and moving around classes

--- wp-content/themes/deed/header.php
+++ wp-content/themes/deed/header.php
@@ -44,13 +44,9 @@
 </head>
 <body class="conatiner-fluid" <?php body_class(); ?>>
 <header class="jumbotron container">
-		<div class="row container header-title-row">
-			<div class="col-sm-12">
+		<div class="row container header-title-row col-sm-12">
 				<img src="http://dev-a-deed-a-week.pantheonsite.io/wp-content/themes/deed/build/images/bracelet300px.png" class="bracelets" alt="picture">
-				<h1>
-					<a class="blog-name" href="/"><?php echo bloginfo('name'); ?></a>
-				</h1>
-			</div>
+				<h1><a class="blog-name" href="/"><?php echo bloginfo('name'); ?></a></h1>
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
