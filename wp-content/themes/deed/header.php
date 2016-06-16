2c0e0082a9d5cdd0cb5c71c20c563366672bdb16
kfmahre <kfmahre@gmail.com>
2016-06-16 19:23:56 -0400

    adding some floats to make the layout work better

--- wp-content/themes/deed/header.php
+++ wp-content/themes/deed/header.php
@@ -46,7 +46,7 @@
 <header class="row container">
 		<div class="row container header-title-row col-sm-12">
 				<img src="http://dev-a-deed-a-week.pantheonsite.io/wp-content/themes/deed/build/images/bracelet300px.png" class="bracelets" alt="picture">
-				<h1><a class="blog-name" href="/"><?php echo bloginfo('name'); ?></a></h1>
+				<h1><?php echo bloginfo('name'); ?></h1>
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
