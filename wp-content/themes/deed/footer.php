751e73f6c42ea2958243019d39a09a67930c64e8
kfmahre <kfmahre@gmail.com>
2016-06-23 13:14:08 -0400

    modifying the footer

--- wp-content/themes/deed/footer.php
+++ wp-content/themes/deed/footer.php
@@ -4,12 +4,24 @@
 		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
 		<footer class="container-fluid">
 			<div class="container">
-				<div class="row-fluid">
-					<div class="span5">
-
-					</div>
-					<div class="span5 offset2">
-
+				<div class="row">
+					<?php if ( has_nav_menu( 'social' ) ) : ?>
+						<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+						</nav><!-- .social-navigation -->
+					<?php endif; ?>
+					<div class="row">
+						<span class="site-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						</span>
 					</div>
 				</div>
 			</div>
