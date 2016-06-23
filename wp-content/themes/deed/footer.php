25ed332802cef0ed0033b859169671850c6435f5
kfmahre <kfmahre@gmail.com>
2016-06-23 13:24:05 -0400

    more modifications to footer

--- wp-content/themes/deed/footer.php
+++ wp-content/themes/deed/footer.php
@@ -1,26 +1,18 @@
 			</div><!-- END CONTAINER -->
 		</div><!-- END FLUID CONTAINER -->
 
-		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+
 		<footer class="container-fluid">
 			<div class="container">
 				<div class="row">
-					<?php if ( has_nav_menu( 'social' ) ) : ?>
-						<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'social',
-							'menu_class'     => 'social-links-menu',
-							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
-							'link_after'     => '</span>',
-						) );
-					?>
-						</nav><!-- .social-navigation -->
-					<?php endif; ?>
+					<nav class="row">
+						<div class="col-sm-12">
+							<?php wp_nav_menu( array( 'theme_location' => 'footer-menu',  'container_class' => 'footer_menu' ) ); ?>
+						</div>
+					</nav>
 					<div class="row">
-						<span class="site-title">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						<span>
+							<h4><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h4>
 						</span>
 					</div>
 				</div>
@@ -28,7 +20,6 @@
 		</footer>
     <!-- WP FOOTER -->
   	<?php wp_footer(); ?>
-	</a>
 
 </body>
 </html>
