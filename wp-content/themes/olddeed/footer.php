5060555aef814a05154590fb5d64770bc565c027
kfmahre <kfmahre@gmail.com>
2017-10-22 14:34:59 -0400

    adding work to my personal github for future record

---
+++ wp-content/themes/olddeed/footer.php
@@ -0,0 +1,22 @@
+		</div><!-- END CONTAINER -->
+		</div><!-- END FLUID CONTAINER -->
+
+		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+		<footer class="container-fluid">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="span5">
+
+					</div>
+					<div class="span5 offset2">
+
+					</div>
+				</div>
+			</div>
+		</footer>
+    <!-- WP FOOTER -->
+  	<?php wp_footer(); ?>
+	</a>
+
+</body>
+</html>
